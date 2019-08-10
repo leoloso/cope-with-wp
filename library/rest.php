99fd34de42e51adbae2bbb7e93ec0695abf3d480
Leo <losoviz@gmail.com>
2019-08-10 14:36:25 +0800

    Removed empty space

--- library/rest.php
+++ library/rest.php
@@ -11,16 +11,15 @@ add_action('rest_api_init', function () {
 });
 function get_post_block_meta($request) 
 {
-    $post = get_post($request['post_id']);
-    if (!$post) {
-      return new WP_Error('empty_post', 'There is no post with this ID', array('status' => 404));
-    }
+  $post = get_post($request['post_id']);
+  if (!$post) {
+    return new WP_Error('empty_post', 'There is no post with this ID', array('status' => 404));
+  }
 
-    $block_data = get_block_data($post->post_content);
-    $block_metadata = get_block_metadata($block_data);
-    $response = new WP_REST_Response($block_metadata);
-    $response->set_status(200);
-
-    return $response;
+  $block_data = get_block_data($post->post_content);
+  $block_metadata = get_block_metadata($block_data);
+  $response = new WP_REST_Response($block_metadata);
+  $response->set_status(200);
+  return $response;
 }
 
